df7aa9715719a535d497790703c52a87c7fe1274
Mahmoud <mahmoud_gamal_abolfotoh@hotmail.com>
2019-07-28 00:01:32 +0200

    Refactor repoTableViewDatasource out of UserRepoController to RepoTableDataSource

---
+++ UserRepos/Controllers/RepoTableDataSource.swift
@@ -0,0 +1,56 @@
+//
+//  RepoTableDataSource.swift
+//  UserRepos
+//
+//  Created by Mahmoud Abolfotoh on 7/27/19.
+//  Copyright © 2019 Mahmoud Abolfotoh. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableDataSource: NSObject,  UITableViewDataSource {
+    var repos = [Repo]()
+    let repoApiManager = RepoAPIManager()
+    var dataChanged: ((_ isSuccess: Bool) -> Void)?
+    
+    init(isTesting: Bool = false) {
+        super.init()
+        if !isTesting {
+            fetchRepos()
+        }
+    }
+
+    func fetchRepos() {
+        repoApiManager.fetchUserRepos {[weak self] (result: Result<[Repo], APIService.APIError>) in
+            switch result {
+            case let .success(repos):
+                self?.repos = repos
+                self?.dataChanged?(true)
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+    
+    func repo(at indexPath: IndexPath) -> Repo {
+        return repos[indexPath.row]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RepoCell.reuseIdentifier, for: indexPath) as? RepoCell else {
+            fatalError("Couldn't deque new repo cell")
+        }
+        let repo = repos[indexPath.row]
+        cell.titleLabel.text = repo.name
+        cell.descriptionLabel.text = repo.description
+        cell.languageLabel.text = repo.language ?? ""
+        cell.forksLabel.text = "\(repo.forksCount)"
+        cell.dateLabel.text = repo.formmatedDate
+        cell.userImageView.url = URL(string: repo.owner.avatarURL)
+        return cell
+    }
+}
--- UserRepos/Controllers/UserReposController.swift
+++ UserRepos/Controllers/UserReposController.swift
@@ -10,61 +10,32 @@ import UIKit
 
 class UserReposController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    let repoApiManager = RepoAPIManager()
     var repos = [Repo]()
+    var dataSource = RepoTableDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
-        fetchRepos()
+        dataSource.dataChanged = { [weak self] isChanged in
+            if isChanged {
+                self?.tableView.reloadData()
+            }
+        }
     }
     
     func setupTableView() {
-        tableView.dataSource = self
+        tableView.dataSource = dataSource
         tableView.delegate = self
         tableView.register(RepoCell.self, forCellReuseIdentifier: RepoCell.reuseIdentifier)
-    }
-    
-    func fetchRepos() {
-        repoApiManager.fetchUserRepos {[weak self] (result: Result<[Repo], APIService.APIError>) in
-            switch result {
-            case let .success(repos):
-                self?.repos = repos
-                self?.title = repos.first?.owner.name
-                self?.tableView.reloadData()
-            case let .failure(error):
-                print(error)
-            }
-        }
     }
 }
 
 extension UserReposController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let repo = repos[indexPath.row]
+        let repo = dataSource.repo(at: indexPath)
         let repoWebController = RepoWebController()
         repoWebController.repo = repo
         navigationController?.pushViewController(repoWebController, animated: true)
         tableView.deselectRow(at: indexPath, animated: false)
-    }
-}
-
-extension UserReposController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repos.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: RepoCell.reuseIdentifier, for: indexPath) as? RepoCell else {
-            fatalError("Couldn't deque new repo cell")
-        }
-        let repo = repos[indexPath.row]
-        cell.titleLabel.text = repo.name
-        cell.descriptionLabel.text = repo.description
-        cell.languageLabel.text = repo.language ?? ""
-        cell.forksLabel.text = "\(repo.forksCount)"
-        cell.dateLabel.text = repo.formmatedDate
-        cell.userImageView.url = URL(string: repo.owner.avatarURL)
-        return cell
     }
 }
