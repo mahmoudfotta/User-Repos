eca00ad94a52077035bc1afded186a13752472b6
Mahmoud <mahmoud_gamal_abolfotoh@hotmail.com>
2019-07-28 01:20:16 +0200

    Add unit test

--- UserReposTests/UserReposTests.swift
+++ UserReposTests/UserReposTests.swift
@@ -10,25 +10,86 @@ import XCTest
 @testable import UserRepos
 
 class UserReposTests: XCTestCase {
-
+    var repoJson = """
+    {
+    "name": "Codextended",
+    "full_name": "JohnSundell/Codextended",
+    "owner": {
+    "login": "JohnSundell",
+    "avatar_url": "https://avatars3.githubusercontent.com/u/2466701?v=4",
+    },
+    "html_url": "https://github.com/JohnSundell/Codextended",
+    "description": "Extensions giving Swift's Codable API type inference super powers 🦸‍♂️🦹‍♀️",
+    "fork": false,
+    "url": "https://api.github.com/repos/JohnSundell/Codextended",
+    "created_at": "2019-04-06T23:33:12Z",
+    "language": "Swift",
+    "forks_count": 36,
+    "forks": 36,
+    }
+"""
+    
+    var repoData: Data!
+    var dataSource: RepoTableDataSource!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        repoData = repoJson.data(using: .utf8)
+        dataSource = RepoTableDataSource(isTesting: true)
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        repoData = nil
+        dataSource = nil
+    }
+    
+    func decodeRepoData() -> Repo? {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        let repo = try? decoder.decode(Repo.self, from: repoData)
+        return repo
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    //MARK:- Repo and Ownwr Tests
+    
+    func testRepoAndOwnerDataDecodedCorrectly() {
+        let repo = decodeRepoData()
+        
+        if let repo = repo {
+            XCTAssertEqual(repo.name, "Codextended")
+            XCTAssertEqual(repo.language, "Swift")
+            XCTAssertEqual(repo.forksCount, 36)
+            XCTAssertEqual(repo.repoURL, "https://github.com/JohnSundell/Codextended")
+            XCTAssertEqual(repo.owner.name, "JohnSundell")
+            XCTAssertEqual(repo.owner.avatarURL, "https://avatars3.githubusercontent.com/u/2466701?v=4")
+        } else {
+            XCTFail("Error decoding repo data")
         }
     }
+    
+    func testRepoDateFormatIsCorrect() {
+        let repo = decodeRepoData()
+        
+        if let repo = repo {
+            XCTAssertEqual(repo.formmatedDate, "2019 04 07")
+        } else {
+            XCTFail("Error decoding repo data")
+        }
+    }
+    
+    //MARK:- Repo DataSource Tests
 
+    func testReposArrayCountIsZeroAtFirst() {
+        XCTAssertEqual(dataSource.repos.count, 0)
+    }
+    
+    func testSettingReposPropertyIsSettingCorrectly() {
+        let repo = decodeRepoData()
+        
+        if let repo = repo {
+            dataSource.repos = [repo]
+            XCTAssertEqual(dataSource.repos.count, 1)
+        } else {
+            XCTFail()
+        }
+    }
 }
